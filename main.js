072a4aab91bccb6f210d592e2e71c696da3510a8
jaysongiroux <46802437+jaysongiroux@users.noreply.github.com>
2020-05-06 11:22:53 -0400

    remove file saver

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 require('electron-reload')(__dirname);
-var FileSaver = require('file-saver');
 
 const path = require('path')
 
